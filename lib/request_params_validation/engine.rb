57865614300662ba97a77588945139c8ac71da18
Felipe Fava <felipe.fava@moove-it.com>
2020-04-18 17:44:23 -0300

    Load definitions on start and include helpers module

--- lib/request_params_validation/engine.rb
+++ lib/request_params_validation/engine.rb
@@ -1,5 +1,18 @@
 module RequestParamsValidation
+  autoload :Definitions, 'request_params_validation/definitions'
+  autoload :Helpers,     'request_params_validation/helpers'
+
   class Engine < ::Rails::Engine
     isolate_namespace RequestParamsValidation
+
+    initializer 'request_params_validation.load_definitions' do
+      RequestParamsValidation::Definitions.load_all
+    end
+
+    initializer 'request_params_validation.add_helpers' do
+      ActiveSupport.on_load :action_controller do
+        include RequestParamsValidation::Helpers
+      end
+    end
   end
 end
