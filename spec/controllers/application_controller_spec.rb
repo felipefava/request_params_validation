b9580929cff3d96034e7df87050f560c9d57d771
Felipe Fava <felipe.fava@moove-it.com>
2020-04-25 20:21:42 -0300

    Add tests for length validator

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,4 +2,5 @@ RSpec.describe ApplicationController do
   include_examples 'validates presence'
   include_examples 'validates type'
   include_examples 'validates inclusion'
+  include_examples 'validates length'
 end
